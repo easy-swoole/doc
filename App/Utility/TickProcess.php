ae3258cbad2d2f38afe73d2664211fb1c647daa6
gaobinzhan <gaobinzhan@gmail.com>
2020-09-01 12:00:42 +0800

    remove doc-system

--- App/Utility/TickProcess.php
+++ App/Utility/TickProcess.php
@@ -7,10 +7,10 @@ namespace App\Utility;
 use App\Model\Document\Doc;
 use EasySwoole\Component\Process\AbstractProcess;
 use EasySwoole\Component\Timer;
-use EasySwoole\DocSystem\DocLib\DocSearchParser;
 use EasySwoole\EasySwoole\Logger;
 use EasySwoole\EasySwoole\Trigger;
 use Swoole\Coroutine;
+use voku\helper\HtmlDomParser;
 
 class TickProcess extends AbstractProcess
 {
@@ -24,10 +24,32 @@ class TickProcess extends AbstractProcess
                 /** @var Doc $doc */
                 foreach ($list as $doc) {
                     //定时生成搜索的json文件
-                    $json = DocSearchParser::parserDoc2JsonUrlMap($doc->getRootPath() . '/', '');
-                    file_put_contents(EASYSWOOLE_ROOT . "/Static/keyword{$doc->getName()}.json", json_encode($json, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+                    $markdownResult = $doc->renderMarkdown('sidebar.md');
+                    $dom = HtmlDomParser::str_get_html($markdownResult->getHtml());
+                    $aList = $dom->find('a');
+
+                    $jsonList = [];
+                    $id = 1;
+                    foreach ($aList as $a) {
+                        $path = $a->href;
+                        $name = $a->getNode()->textContent;
+
+                        $link = $path;
+                        if (substr($link, -3) == '.md') {
+                            $link = substr($path, 0, -3) . '.html';
+                        }
+
+                        $jsonList[] = [
+                            'id' => $id,
+                            'title' => strtolower($name),
+                            'content' => strip_tags($doc->renderMarkdown($path)->getHtml()),
+                            'link' => '/' . $link,
+                        ];
+                        $id++;
+                    }
+                    file_put_contents(EASYSWOOLE_ROOT . "/Static/keyword{$doc->getName()}.json", json_encode($jsonList, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
                 }
-            }catch (\Throwable $throwable){
+            } catch (\Throwable $throwable) {
                 Trigger::getInstance()->throwable($throwable);
             }
 
