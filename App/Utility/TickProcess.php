d8675bb068207249398c1c85582eb2e9c3832669
XueSi <1592328848@qq.com>
2021-02-04 10:10:53 +0800

    up doc: 优化缓存搜索内容时间

--- App/Utility/TickProcess.php
+++ App/Utility/TickProcess.php
@@ -18,22 +18,18 @@ class TickProcess extends AbstractProcess
 {
     protected function run($arg)
     {
-        go(function () {
-            while (1) {
-                $list = Config::getInstance()->getConf("DOC.ALLOW_LANGUAGE");
-                try {
-                    foreach ($list as $dir => $value) {
-                        $json = DocSearchParser::parserDoc2JsonUrlMap(EASYSWOOLE_ROOT, "{$dir}");
-                        file_put_contents(EASYSWOOLE_ROOT . "/Static/keyword{$dir}.json", json_encode($json, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
-                    }
-                } catch (\Throwable $throwable) {
-                    Trigger::getInstance()->throwable($throwable);
-                }
-            }
-        });
-
-
         Timer::getInstance()->loop(30 * 1000, function () {
+            // 写入搜索内容json
+            $list = Config::getInstance()->getConf("DOC.ALLOW_LANGUAGE");
+            try {
+                foreach ($list as $dir => $value) {
+                    $json = DocSearchParser::parserDoc2JsonUrlMap(EASYSWOOLE_ROOT, "{$dir}");
+                    file_put_contents(EASYSWOOLE_ROOT . "/Static/keyword{$dir}.json", json_encode($json, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+                }
+            } catch (\Throwable $throwable) {
+                Trigger::getInstance()->throwable($throwable);
+            }
+
             //本项目是git克隆下来的，因此自动同步
             $exec = "cd " . EASYSWOOLE_ROOT . "; git pull";
             System::exec($exec);
